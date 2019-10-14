f45fcb3d8f670eada9f346c9edc37e4e7837fa28
JustAidanP <justaidanp@gmail.com>
2019-10-14 12:17:50 +0100

    Added namespaces and a makefile

---
+++ Swift/Namespaces.swift
@@ -0,0 +1,26 @@
+//------Structures------
+//Defines a namespace, this can be used for jumps
+struct Namespace{
+    //Stores the name of the namespace
+    var name:String
+    //Stores any children namespaces
+    var namespaces:[Namespace] = [Namespace]()
+    //Stores the labels of the namespace
+    var labels:[String:NodeState] = [String:NodeState]()
+}
+
+//Defines a NodeState, this stores information about the process at the point of storage
+struct NodeState{
+    //------Variables------
+    //Stores the nodeTree
+    var nodeTree:Node
+    //Stores the indexStack at that point
+    var indexStack:[Int]
+    //------Procedures/Functions------
+    //Gets a clean NodeState
+    //Arguments:    -A node tree    -Node
+    //Returns:  An empty nodestate  -NodeState
+    static func clean(_nodeTree:Node) -> NodeState{
+        return NodeState(nodeTree:_nodeTree, indexStack: [0, 0])
+    }
+}
